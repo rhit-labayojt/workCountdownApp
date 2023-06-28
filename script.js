b9aaa494dbddd21b409aab5b9c85199e44b0b2bb
rhit-labayojt <labayojt@rose-hulman.edu>
2023-06-28 12:28:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ function startCountdown() {
     targetEndTime.setDate(targetEndTime.getDate() + 1);
   }
 
-  if (btnPrsd == true) {
-    targetTime.setDate(targetTime.getDate() + 1);
-    targetEndTime.setDate(targetEndTime.getDate() + 1);
-  }
+  // if (btnPrsd == true) {
+  //   targetTime.setDate(targetTime.getDate() + 1);
+  //   targetEndTime.setDate(targetEndTime.getDate() + 1);
+  // }
 
   // Start the countdown interval
   setInterval(updateCountdown, 1000, targetTime);
@@ -63,7 +63,7 @@ function updateEndCountdown(targetEndTime) {
 document.getElementById("myButton").addEventListener("click", function () {
   // Code to be executed when the button is clicked
   alert("Enjoy your day off!");
-  btnPrsd = true
+  // btnPrsd = true;
   startCountdown;
 });
 
