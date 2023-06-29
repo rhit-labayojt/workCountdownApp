5e73edbd4d185c927ce2f428fb908786d688feae
rhit-labayojt <labayojt@rose-hulman.edu>
2023-06-29 14:31:42 -0500

    combine countdown update func

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let btnPrsd = false;
 
 function startCountdown() {
-  // Set the target time for the work start countdown (e.g., 1:20 PM)
+  // Set the target time for the work start countdown (1:20 PM)
   const targetTime = new Date();
   targetTime.setHours(13, 20, 0); // Set the desired hour, minute, and second
 
-  // Set the target time for the work end countdown (e.g., 10:30 PM)
+  // Set the target time for the work end countdown (10:30 PM)
   const targetEndTime = new Date();
   targetEndTime.setHours(22, 30, 0); // Set the desired hour, minute, and second
 
@@ -29,35 +29,54 @@ function startCountdown() {
   }
 
   // Start the countdown interval
-  setInterval(updateCountdown, 1000, targetTime);
-  setInterval(updateEndCountdown, 1000, targetEndTime);
+  setInterval(updateCountdown, 1000, targetTime, targetEndTime);
+  // setInterval(updateEndCountdown, 1000, targetEndTime);
 }
 
-function updateCountdown(targetTime) {
+function updateCountdown(targetTime, targetEndTime) {
   const currentTime = new Date();
-  const distance = targetTime - currentTime;
+  const distance1 = targetTime - currentTime;
+  const distance2 = targetTime - currentTime;
 
   // Calculate hours, minutes, and seconds
-  const hours = Math.floor(distance / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const hours1 = Math.floor(distance1 / (1000 * 60 * 60));
+  const minutes1 = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds1 = Math.floor((distance1 % (1000 * 60)) / 1000);
+
+  const hours2 = Math.floor(distance2 / (1000 * 60 * 60));
+  const minutes2 = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds2 = Math.floor((distance2 % (1000 * 60)) / 1000);
 
   // Display the countdown
-  document.getElementById('countdown').innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+  document.getElementById('countdown').innerHTML = `${hours1}h ${minutes1}m ${seconds1}s`;
+  document.getElementById('endCountdown').innerHTML = `${hours2}h ${minutes2}m ${seconds2}s`;
 }
 
-function updateEndCountdown(targetEndTime) {
-  const currentTime = new Date();
-  const distance = targetEndTime - currentTime;
+// function updateCountdown(targetTime) {
+//   const currentTime = new Date();
+//   const distance = targetTime - currentTime;
 
-  // Calculate hours, minutes, and seconds
-  const hours = Math.floor(distance / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Calculate hours, minutes, and seconds
+//   const hours = Math.floor(distance / (1000 * 60 * 60));
+//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the countdown
-  document.getElementById('endCountdown').innerHTML = `${hours}h ${minutes}m ${seconds}s`;
-}
+//   // Display the countdown
+//   document.getElementById('countdown').innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+// }
+
+// function updateEndCountdown(targetEndTime) {
+//   const currentTime = new Date();
+//   const distance = targetEndTime - currentTime;
+
+//   // Calculate hours, minutes, and seconds
+//   const hours = Math.floor(distance / (1000 * 60 * 60));
+//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Display the countdown
+//   document.getElementById('endCountdown').innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+// }
 
 document.getElementById("myButton").addEventListener("click", function () {
   // Code to be executed when the button is clicked
